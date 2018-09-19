68ab70b887b12cb1992ec39978a7dfe78eea5026
julianmnst <julians.mg@gmail.com>
2018-09-19 03:19:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Tuit
+
+In order to use, clone the repository and run 'composer install'.
+For starting the project, run:
+
+	php -S localhost:8080 -t public index.php
+
+The requested endpoint is:
+
+	http://localhost:8080/tweets/{username}
+
+But you can also checkout the [index page](http://localhost:8080).
+
+I used [this](https://github.com/J7mbo/twitter-api-php/blob/master/TwitterAPIExchange.php) class to consume the TwitterAPI.
---
+++ src/routes.php
@@ -0,0 +1,65 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+require __DIR__ . '/../TwitterAPIExchange.php';
+
+// Routes
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+  // Sample log message
+  $this->logger->info("Slim-Skeleton '/' route");
+
+  // Render index view
+  return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->get('/tweets/[{name}]', function(Request $request, Response $response, array $args) {
+
+  if (is_null($args['name'])) {
+    $response->getBody()->write('
+      Please enter a <strong>valid</strong> Twitter Username <br>
+      For example: <a href="http://localhost:8080/tweets/realDonaldTrump">http://localhost:8080/tweets/<strong>realDonaldTrump</strong></a>
+    ');
+
+    return $response->withStatus(400);
+  }
+
+  $settings = array(
+    'oauth_access_token' => "22108512-lxIdVWtFHwMiIkH5sN7o8vkLZITEZnUc93nqttZBW",
+    'oauth_access_token_secret' => "4KchGiAgD45jGVCLrkUxJFXf8LHP4QkBZP1sqYQoZB2bA",
+    'consumer_key' => "C0cZJ3wFDryw01wJHYhLibbIO",
+    'consumer_secret' => "6Uk2BoVke6MXOBg1Y5yKfSqKePzdafAu1xznUaJKbUTxZlaw0b"
+  );
+
+  $url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+
+  $fields = '?screen_name='.$args['name'].'&count=10';
+
+  $twitter = new TwitterAPIExchange($settings);
+
+  $data = $twitter->setGetfield($fields)->buildOauth($url, "GET")->performRequest();
+
+  $tweets = json_decode($data);
+
+  $result = [];
+
+  foreach($tweets as $tweet){
+    $t = [];
+
+    $t['text'] = $tweet->text;
+    $t['created_at'] = $tweet->created_at;
+
+    if ($tweet->in_reply_to_user_id && $tweet->in_reply_to_screen_name) {
+      $t['in_reply'] = array(
+        'id' => $tweet->in_reply_to_user_id,
+        'name' => $tweet->in_reply_to_screen_name
+      );
+    }
+
+    array_push($result, $t);
+  }
+
+  return $response->withJson($result);
+});
