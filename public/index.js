3dfa09bbbe858fe68f026e314af371a420f27e2e
julianmnst <julians.mg@gmail.com>
2019-05-15 20:27:57 -0300

    animating tweets

---
+++ public/index.js
@@ -0,0 +1,54 @@
+const devUrl = 'http://localhost:8080/tweets/';
+const devMode = true;
+var baseUrl = 'https://tuit-fetcher.herokuapp.com/tweets/',
+container = document.getElementById('container'),
+username = document.getElementById('username'),
+get_tweets = document.getElementById('get-tweets')
+
+if (devMode) {
+  baseUrl = devUrl;
+}
+
+function callAjax(url, callback){
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function(){
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+      callback(xmlhttp.responseText);
+    }
+  }
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
+
+function getTweets() {
+  container.innerHTML = '';
+  callAjax(baseUrl + username.value, function(response){
+    var tweets = JSON.parse(response);
+    tweets = tweets.map(function(tweet) {
+      return '<div class="hidden">'
+      + tweet.text
+      + '</div>';
+    })
+    container.innerHTML = tweets.join('')
+    revealList(tweets, 200);
+  });
+}
+
+get_tweets.addEventListener('click', function(e) {
+  getTweets();
+})
+
+function revealList(list, interval) {
+  let counter = 0;
+
+  var intervalId = setInterval(loop, interval)
+
+  function loop() {
+    document.getElementsByClassName('hidden')[counter].classList.add('tweet');
+    counter++;
+
+    if (counter == (list.length)) {
+      clearInterval(intervalId);
+    }
+  }
+}
