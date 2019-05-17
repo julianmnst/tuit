ae46c1e0c827fdb418a72da88195e5450e12c78b
Julian Mansueto <julians.mg@gmail.com>
2019-05-17 06:13:20 +0000

    added about page

--- src/routes.php
+++ src/routes.php
@@ -15,6 +15,14 @@ $app->get('/', function (Request $request, Response $response, array $args) {
   return $this->renderer->render($response, 'index.phtml', $args);
 });
 
+$app->get('/about', function (Request $request, Response $response, array $args) {
+  // Sample log message
+  $this->logger->info("Slim-Skeleton '/' route");
+
+  // Render index view
+  return $this->renderer->render($response, 'about.phtml', $args);
+});
+
 $app->get('/tweets/[{name}]', function(Request $request, Response $response, array $args) {
 
   // If no username is provided, return error and instructions
